c84a39289286f462227d1812d272183f96f948b4
MKinanA <mkinanahsan@gmail.com>
2023-10-27 14:30:48 +0700

    searching ayat in surat view is now possible

--- js/surat.js
+++ js/surat.js
@@ -1,4 +1,5 @@
-let isi_surat, nomor_surat, DEBUG_MODE;
+let isi_surat, nomor_surat, DEBUG_MODE, hasil_cari = [];
+const cari_ayat = document.querySelector('nav form input.form-control');
 DEBUG_MODE = true;
 
 function waitFor(conditionFunction) {
@@ -46,6 +47,49 @@ function get_isi_surat(nomor_surat) {
     }).catch(error => loading(`${error}, check your connection and refresh`));
 };
 
+function display_cari_ayat() {
+    debuglog('display_cari_surat() is called');
+    if (cari_ayat.value == '') {
+        debuglog('query is empty, using display_isi_surat instead');
+        display_isi_surat();
+    } else {
+        let card_ayat = '';
+        hasil_cari = [];
+        const query = cari_ayat.value.toLowerCase();
+        debuglog('query is ' + query);
+        isi_surat.ayat.forEach(ayat => {
+            if (ayat.ar.toLowerCase().includes(query) || ayat.tr.toLowerCase().includes(query) || ayat.idn.toLowerCase().includes(query) || ayat.nomor === parseInt(query)) {
+                function highlight_search(text, search) {
+                    const regex = new RegExp(search, 'gi');
+                    return text.replace(regex, (match) => `<span class="highlighted">${match}</span>`);
+                };
+                card_ayat += `
+<div class="card mb-3 card-ayat" id="ayat-${ayat.nomor}">
+    <div class="card-body">
+        <p class="nomor">${highlight_search(String(ayat.nomor), query)}</p>
+        <p class="text-end arab">${highlight_search(ayat.ar, query)}</p>
+        <p class="latin">${highlight_search(ayat.tr, query)}</p>
+        <p class="arti">${highlight_search(ayat.idn, query)}</p>
+    </div>
+</div>`;
+                hasil_cari.push(`ayat-${ayat.nomor}`);
+            } else {
+                card_ayat += `
+<div class="card mb-3 card-ayat card-ayat-inactive" id="ayat-${ayat.nomor}">
+    <div class="card-body">
+        <p class="nomor">${ayat.nomor}</p>
+        <p class="text-end arab">${ayat.ar}</p>
+        <p class="latin">${ayat.tr}</p>
+        <p class="arti">${ayat.idn}</p>
+    </div>
+</div>`;
+            };
+        });
+        document.querySelector('body>.container .row .col .ayat').innerHTML = card_ayat;
+        if (hasil_cari.length >= 1) {document.getElementById(hasil_cari[0]).scrollIntoView();};
+    };
+};
+
 function display_isi_surat() {
     const ayat_surat = isi_surat.ayat;
     let card_surat_title = `
@@ -59,19 +103,19 @@ function display_isi_surat() {
 </div>`, card_ayat = '';
     ayat_surat.forEach(ayat => {
         card_ayat += `
-<div class="card mb-3 card-ayat" id="ayat-${ayat.id}">
+<div class="card mb-3 card-ayat" id="ayat-${ayat.nomor}">
     <div class="card-body">
         <p class="nomor">${ayat.nomor}</p>
         <p class="text-end arab">${ayat.ar}</p>
         <p class="latin">${ayat.tr}</p>
         <p class="arti">${ayat.idn}</p>
     </div>
-</div>`
+</div>`;
     });
     document.querySelector('head>title').innerHTML = `${isi_surat.nomor}. ${isi_surat.nama_latin} ${isi_surat.nama} | Qur'an`;
     document.querySelector('nav h1.navbar-brand').innerHTML = `<span><span class="bi bi-caret-left-fill back" title="Home" onclick="history.back()"></span>${isi_surat.nomor}. ${isi_surat.nama_latin}</span><span class="text-end arab">${isi_surat.nama}</span>`;
     document.querySelector('body>.container').innerHTML = '<div class="row"><div class="col 12"></div></div>';
-    document.querySelector('body>.container .row .col').innerHTML = card_surat_title + card_ayat;
+    document.querySelector('body>.container .row .col').innerHTML = card_surat_title + `<div class='ayat'>${card_ayat}</div>`;
 };
 
 function play_surat_audio() {
