733734a505c7561687c6e9ce321e1c560fa22805
MKinanA <mkinanahsan@gmail.com>
2023-12-12 14:48:14 +0700

    site is now an installable PWA!

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@ const cari_surat = document.querySelector('nav form input.form-control');
 DEBUG_MODE = true;
 isi_surat = Array(114);
 
+if ('serviceWorker' in navigator) { // registering Service Worker
+    navigator.serviceWorker.register('sw.js')
+    .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+        console.log('Service Worker registration failed:', error);
+    })
+};
+
 function waitFor(conditionFunction) {
     const poll = resolve => {
       if(conditionFunction()) resolve();
